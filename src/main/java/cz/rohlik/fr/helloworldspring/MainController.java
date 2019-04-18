962a42c7c3b83e4b796d949bad12f6c8a22855cd
jan.splichal <jan.splichal@rohlik.cz>
2019-04-18 13:41:03 +0200

    init commit

---
+++ src/main/java/cz/rohlik/fr/helloworldspring/MainController.java
@@ -0,0 +1,14 @@
+package cz.rohlik.fr.helloworldspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+}
---
+++ src/test/java/cz/rohlik/fr/helloworldspring/HelloworldspringApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.rohlik.fr.helloworldspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloworldspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
